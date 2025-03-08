303b55f21197f4e86aae21e19daa774df4838d8a
nikmosi <i@nikmosi.ru>
2025-03-08 13:47:36 +0700

    init develop shell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@args:
+    args.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ uv ];
+          shellHook = ''exec fish'';
+        };
+      }
+    );
+}
